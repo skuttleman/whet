644c7ca20211095ce863775dfb2dd82ca22e5519
Ben Allred <skuttleman@gmail.com>
2024-01-15 15:26:54 -0700

    refactor

--- src/whet/impl/middleware.clj
+++ src/whet/impl/middleware.clj
@@ -22,19 +22,17 @@
 
 (defn ^:private with-edn [handler]
   (fn [req]
-    (if (:hydrating? req)
-      (handler req)
-      (let [content-type (or (ring.req/content-type req)
-                             "application/edn")
-            response (-> req
-                         (cond-> (string/starts-with? content-type "application/edn")
-                                 (update :body read-edn))
-                         handler)]
-        (cond-> response
-          (and (nil? (get-in response [:headers "content-type"]))
-               (some? (:body response)))
-          (-> (assoc-in [:headers "content-type"] "application/edn")
-              (update :body pr-str)))))))
+    (let [content-type (or (ring.req/content-type req)
+                           "application/edn")
+          response (-> req
+                       (cond-> (string/starts-with? content-type "application/edn")
+                               (update :body read-edn))
+                       handler)]
+      (cond-> response
+        (and (nil? (get-in response [:headers "content-type"]))
+             (some? (:body response)))
+        (-> (assoc-in [:headers "content-type"] "application/edn")
+            (update :body pr-str))))))
 
 (defn ^:private with-routing [handler routes]
   (fn [{:keys [route uri] :as req}]
